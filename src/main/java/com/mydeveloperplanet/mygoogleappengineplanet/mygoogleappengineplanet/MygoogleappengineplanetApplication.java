3470c4b2521f47ff6d5b6a5b739ea3fa8b7c661c
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2019-03-09 12:26:53 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/mygoogleappengineplanet/mygoogleappengineplanet/MygoogleappengineplanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.mygoogleappengineplanet.mygoogleappengineplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygoogleappengineplanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygoogleappengineplanetApplication.class, args);
+	}
+
+}
