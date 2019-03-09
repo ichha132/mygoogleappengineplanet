2b18d1375ab4213bc2c59ea3ed2f62d2d90a0461
Gunter Rotsaert <gunter@mydeveloperplanet.com>
2019-03-09 12:55:42 +0100

    Fix CamelCase

--- src/main/java/com/mydeveloperplanet/mygoogleappengineplanet/mygoogleappengineplanet/MyGoogleAppEnginePlanetApplication.java
+++ src/main/java/com/mydeveloperplanet/mygoogleappengineplanet/mygoogleappengineplanet/MyGoogleAppEnginePlanetApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MygoogleappengineplanetApplication {
+public class MyGoogleAppEnginePlanetApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MygoogleappengineplanetApplication.class, args);
+		SpringApplication.run(MyGoogleAppEnginePlanetApplication.class, args);
 	}
 
 }
